68ede20caa3eb1471bcc7c633a345f4b9c7c97cc
DMichelR <100800380+DMichelR@users.noreply.github.com>
2022-03-02 18:43:08 -0400

    main TicTacToe

--- Practica7/app/src/main/java/Practica7/App.java
+++ Practica7/app/src/main/java/Practica7/App.java
@@ -8,20 +8,20 @@ public class App {
 
     public static void main(String[] args) {
         //Ejercicio 1
-        /* int [][] matrix = new int[4][4];
+        int [][] matrix = new int[4][4];
         int option;
         boolean matrixFilled = false; 
         do{
             option = Matrix.DisplayMenu(matrixFilled);
             Matrix.OptionCases(option, matrix, matrixFilled);
         }while(option != 7);
-        System.out.println("Hasta Luego ;)"); */
+        System.out.println("Hasta Luego ;)");
 
 
 
 
         //MAGIC SQUARE************************
-		int length = Magic_square.SetLength();
+		/* int length = Magic_square.SetLength();
 
         int[][] magicSquare = Magic_square.FillSquare(length);
 
@@ -29,11 +29,29 @@ public class App {
 
 		Magic_square.Print(length, magicSquare);
 		
-		Magic_square.KnowSum(magicSquare);
+		Magic_square.KnowSum(magicSquare); */
 
 
 
         //3 en raya *********
+        /* TicTacToe game = new TicTacToe();
+        int row, column;
+        boolean correct, validPosition;
+
+        while (!game.endofgame()) {
+            do{
+                game.showCurrentTurn()
+                game.showBoard()
+                correct = false
+                row = game.setRow();
+                column = game.setColumn();
+                validPosition = game.validatePosition(row, column);
+                if (validatePosition){
+                    if(game)
+                }
+
+            }
+        } */
 	}
 
 }
